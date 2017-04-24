40180093512860544a3cbaa48e28b5d0d7a785ea
Juan Arillo <juanarillo@outlook.com>
2017-04-24 11:11:09 +0200

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,20 +15,36 @@ class NewVisitorTest(unittest.TestCase):
         # Checking the homepage of the to-do app.
         self.browser.get('http://localhost:8000')
 
-        # The page title and header must mention to-do lists
+        # # The page title and header must mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # Someone is invited to enter a to-do item straight away
+        # # Someone is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # The person types "Buy peacock feathers" into a text box
         # (This person hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-        # When he hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
+        # # When he hits enter, the page updates, and now the page lists
+        # # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is still a text box inviting him to add another item. she
         # enters "Use peacock feathers to make a fly" (He is very methodical)
+        self.fail('Finish the test!')
 
         # The page update again, and now shows both items on her list
 
